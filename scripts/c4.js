7e54c9bc320a3f59ed1cd190338ac9b8a0c9a065
StefChou <stojkovski.stefan@gmail.com>
2016-12-15 12:16:02 +0100

    Events

---
+++ scripts/c4.js
@@ -0,0 +1,13 @@
+// for(let i = 100; i > 0; i--){
+// 	if(i % 2 == 0){
+// 		console.log(i);
+// 	}
+// }
+
+
+// for(let i = 100; i > 0; i = i - 3){
+// 	if(i % 2 == 0){
+// 		console.log(i);
+// 	}
+// }
+
